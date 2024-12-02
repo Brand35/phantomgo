260b7201de3df9399c448699c116f9f223148566
b.s <brandou35000@gmail.com>
2024-12-02 16:57:54 +0100

    config cable for production && change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 Game.destroy_all
-# User.destroy_all
+User.destroy_all
 
-# noir = User.create!(email: "noir@gmail.com", password: "123456")
+noir = User.create!(email: "noir@gmail.com", password: "123456")
 
-# blanc = User.create!(email: "blanc@gmail.com", password: "123456")
+blanc = User.create!(email: "blanc@gmail.com", password: "123456")
 
-# Game.create!(
-#   black_player: noir,
-#   white_player: blanc,
-#   current_player: noir)
-#
 Game.create!(
-  black_player: User.first,
-  white_player: User.last,
-  current_player: User.first)
+  black_player: noir,
+  white_player: blanc,
+  current_player: noir)
+#
+# Game.create!(
+#   black_player: User.first,
+#   white_player: User.last,
+#   current_player: User.first)
